19a64c572b1830fa6d9830422015a042f6c0c13d
Jake McGinty <me@jake.su>
2021-08-25 23:05:24 +0900

    add netlink code to get viable candidate link addresses

--- shared/src/netlink.rs
+++ shared/src/netlink.rs
@@ -1,13 +1,11 @@
 use ipnetwork::IpNetwork;
 use netlink_packet_core::{
-    NetlinkMessage, NetlinkPayload, NLM_F_ACK, NLM_F_CREATE, NLM_F_EXCL, NLM_F_REQUEST,
+    NetlinkMessage, NetlinkPayload, NLM_F_ACK, NLM_F_CREATE, NLM_F_EXCL,
+    NLM_F_REQUEST,
 };
-use netlink_packet_route::{
-    address, constants::*, link, route, AddressHeader, AddressMessage, LinkHeader, LinkMessage,
-    RouteHeader, RouteMessage, RtnlMessage, RTN_UNICAST, RT_SCOPE_LINK, RT_TABLE_MAIN,
-};
+use netlink_packet_route::{AddressHeader, AddressMessage, LinkHeader, LinkMessage, RTN_UNICAST, RT_SCOPE_LINK, RT_TABLE_MAIN, RouteHeader, RouteMessage, RtnlMessage, address, constants::*, link::{self, nlas::State}, route};
 use netlink_sys::{protocols::NETLINK_ROUTE, Socket, SocketAddr};
-use std::io;
+use std::{io, net::{IpAddr, Ipv4Addr}};
 use wgctrl::InterfaceName;
 
 fn if_nametoindex(interface: &InterfaceName) -> Result<u32, io::Error> {
@@ -23,7 +21,7 @@ fn if_nametoindex(interface: &InterfaceName) -> Result<u32, io::Error> {
 fn netlink_call(
     message: RtnlMessage,
     flags: Option<u16>,
-) -> Result<NetlinkMessage<RtnlMessage>, io::Error> {
+) -> Result<Vec<NetlinkMessage<RtnlMessage>>, io::Error> {
     let mut req = NetlinkMessage::from(message);
     req.header.flags = flags.unwrap_or(NLM_F_REQUEST | NLM_F_ACK | NLM_F_EXCL | NLM_F_CREATE);
     req.finalize();
@@ -43,14 +41,32 @@ fn netlink_call(
         ));
     }
 
-    let n_received = socket.recv(&mut buf[..], 0).unwrap();
-    let response = NetlinkMessage::<RtnlMessage>::deserialize(&buf[..n_received])
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
-    log::trace!("netlink response: {:?}", response);
-    if let NetlinkPayload::Error(e) = response.payload {
-        return Err(e.to_io());
+    let mut responses = vec![];
+    'outer: loop {
+        let n_received = socket.recv(&mut buf[..], 0).unwrap();
+        let mut offset = 0;
+        'inner: loop {
+            let bytes = &buf[offset..];
+            let response = NetlinkMessage::<RtnlMessage>::deserialize(bytes)
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+            if let NetlinkPayload::Error(e) = response.payload {
+                return Err(e.to_io());
+            }
+            responses.push(response.clone());
+            log::trace!("netlink response: {:?}", response);
+            if (req.header.flags & NLM_F_DUMP) == 0 || response.payload == NetlinkPayload::Done {
+                // We've parsed all parts of the response and can leave the loop.
+                break 'outer;
+            }
+            offset += response.header.length as usize;
+            if offset == n_received || response.header.length == 0 {
+                // We've fully parsed the datagram, but there may be further datagrams
+                // with additional netlink response parts.
+                break 'inner;
+            }
+        }
     }
-    Ok(response)
+    Ok(responses)
 }
 
 pub fn set_up(interface: &InterfaceName, mtu: u32) -> Result<(), io::Error> {
@@ -125,5 +141,76 @@ pub fn add_route(interface: &InterfaceName, cidr: IpNetwork) -> Result<bool, io:
         Ok(_) => Ok(true),
         Err(e) if e.kind() == io::ErrorKind::AlreadyExists => Ok(false),
         Err(e) => Err(e),
+    }
+}
+
+pub fn get_local_addrs() -> Result<Vec<IpAddr>, io::Error> {
+    let link_responses = netlink_call(
+        RtnlMessage::GetLink(LinkMessage::default()),
+        Some(NLM_F_DUMP | NLM_F_REQUEST),
+    )?;
+    let links = link_responses
+        .into_iter()
+        // Filter out non-link messages
+        .filter_map(|response| match response {
+            NetlinkMessage {
+                payload: NetlinkPayload::InnerMessage(RtnlMessage::NewLink(link)),
+                ..
+            } => Some(link),
+            _ => None,
+        })
+        // Filter out loopback links
+        .filter_map(|link| if link.header.flags & IFF_LOOPBACK == 0 {
+                Some(link.nlas)
+            } else {
+                None
+            })
+        // Find and filter out addresses for interfaces
+        .filter(|nlas| nlas.iter().any(|nla| nla == &link::nlas::Nla::OperState(State::Up)))
+        .filter_map(|nlas| nlas.iter().find_map(|nla| match nla {
+            link::nlas::Nla::IfName(name) => Some(name.clone()),
+            _ => None,
+        }))
+        .collect::<Vec<_>>();
+    let addr_responses = netlink_call(
+        RtnlMessage::GetAddress(AddressMessage::default()),
+        Some(NLM_F_DUMP | NLM_F_REQUEST),
+    )?;
+    let addrs = addr_responses
+        .into_iter()
+        // Filter out non-link messages
+        .filter_map(|response| match response {
+            NetlinkMessage {
+                payload: NetlinkPayload::InnerMessage(RtnlMessage::NewAddress(addr)),
+                ..
+            } => Some(addr),
+            _ => None,
+        })
+        // Filter out non-global-scoped addresses
+        .filter_map(|link| if link.header.scope == RT_SCOPE_UNIVERSE {
+                Some(link.nlas)
+            } else {
+                None
+            })
+        // Only select addresses for helpful links
+        .filter(|nlas| nlas.iter().any(|nla| matches!(nla, address::nlas::Nla::Label(label) if links.contains(&label))))
+        .filter_map(|nlas| nlas.iter().find_map(|nla| match nla {
+            // TODO(jake): support IPv6 addresses as well.
+            address::nlas::Nla::Address(name) if name.len() == 4 => {
+                Some(IpAddr::V4(Ipv4Addr::new(name[0], name[1], name[2], name[3])))
+            },
+            _ => None,
+        }))
+        .collect::<Vec<_>>();
+    Ok(addrs)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_local_addrs() {
+        get_local_addrs().unwrap();
     }
 }
