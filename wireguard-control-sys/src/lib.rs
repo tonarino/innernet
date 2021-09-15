636eafa21f0ad2ad0e2be377ae59b5031ef71fa3
Matěj Laitl <matej@laitl.cz>
2021-09-15 16:22:58 +0200

    Fix clippy lints in tests

--- wireguard-control-sys/src/lib.rs
+++ wireguard-control-sys/src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+// https://github.com/rust-lang/rust-bindgen/issues/1651
+#![allow(deref_nullptr)]
 
 #[cfg(target_os = "linux")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
