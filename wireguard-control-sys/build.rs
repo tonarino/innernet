b5ebb8e8d7878073ca58a45a0dc78ac53d403b7f
Jake McGinty <me@jake.su>
2021-11-21 15:33:03 +0900

    wireguard-control-sys: use pregenerated bindings
    cuts down on total dependencies and build time goes from ~8s -> ~2s
    (on my desktop).

--- wireguard-control-sys/build.rs
+++ wireguard-control-sys/build.rs
@@ -3,21 +3,29 @@ mod linux {
     use std::{env, path::PathBuf};
 
     pub fn build_bindings() {
-        let bindings = bindgen::Builder::default()
-            .rust_target(bindgen::RustTarget::Stable_1_40)
-            .derive_default(true)
-            .header("c/wireguard.h")
-            .impl_debug(true)
-            .allowlist_function("wg_.*")
-            .bitfield_enum("wg_peer_flags")
-            .bitfield_enum("wg_device_flags");
-
-        let bindings = bindings.generate().expect("Unable to generate bindings");
-
         let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-        bindings
-            .write_to_file(out_path.join("bindings.rs"))
-            .expect("Couldn't write bindings!");
+
+        #[cfg(feature = "buildtime_bindgen")]
+        {
+           let bindings = bindgen::Builder::default()
+                .rust_target(bindgen::RustTarget::Stable_1_40)
+                .derive_default(true)
+                .header("c/wireguard.h")
+                .impl_debug(true)
+                .allowlist_function("wg_.*")
+                .bitfield_enum("wg_peer_flags")
+                .bitfield_enum("wg_device_flags");
+
+            let bindings = bindings.generate().expect("Unable to generate bindings");
+            bindings
+                .write_to_file(out_path.join("bindings.rs"))
+                .expect("Couldn't write bindings!");
+        }
+        #[cfg(not(feature = "buildtime_bindgen"))]
+        {
+            std::fs::copy("bindgen-bindings/bindings.rs", out_path.join("bindings.rs"))
+                .expect("Could not copy bindings to output directory");
+        }
     }
 
     pub fn build_library() {
