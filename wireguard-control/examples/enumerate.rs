a03fb8bdc922343bba6c3c89c3a26b1d1f13fe89
Jake McGinty <me@jake.su>
2022-01-05 19:31:03 -0600

    make enumerate example compilable

--- wireguard-control/examples/enumerate.rs
+++ wireguard-control/examples/enumerate.rs
@@ -1,6 +1,12 @@
 use wireguard_control::{Backend, Device};
 
+
+#[cfg(target_os = "linux")]
+const BACKEND: Backend = Backend::Kernel;
+#[cfg(not(target_os = "linux"))]
+const BACKEND: Backend = Backend::Userspace;
+
 fn main() {
-    let devices = Device::list(Backend::Kernel).unwrap();
+    let devices = Device::list(BACKEND).unwrap();
     println!("{:?}", devices);
 }
