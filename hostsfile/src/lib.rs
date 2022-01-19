f2c1a0da6b12d9ec4fac8707223e70aa21f17715
Jake McGinty <me@jake.su>
2022-01-18 22:03:31 -0600

    hostsfile: add pre-emptive debug logging

--- hostsfile/src/lib.rs
+++ hostsfile/src/lib.rs
@@ -253,8 +253,14 @@ impl HostsBuilder {
         }
 
         match Self::write_and_swap(&temp_path, &hosts_path, &s) {
-            Err(_) => Self::write_clobber(&hosts_path, &s),
-            _ => Ok(()),
+            Err(_) => {
+                log::debug!("wrote hosts file with the clobber fallback strategy");
+                Self::write_clobber(&hosts_path, &s)
+            },
+            _ => {
+                log::debug!("wrote hosts file with the write-and-swap strategy");
+                Ok(())
+            }
         }
     }
 
