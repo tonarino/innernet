0fb5dec1ba8668f3fc9c27ecd9594e0dfe8de243
Ryo Kawaguchi <ryo@kawagu.ch>
2024-04-18 22:55:43 +0900

    Make docker-tests less verbose and easier to add new tests

--- docker-tests/start-client.sh
+++ docker-tests/start-client.sh
@@ -2,6 +2,7 @@
 set -e
 
 INTERFACE="${INTERFACE:-innernet}"
+
 innernet $INNERNET_ARGS install \
     --name "$INTERFACE" \
     --delete-invite \
@@ -9,6 +10,10 @@ innernet $INNERNET_ARGS install \
     /app/invite.toml
 
 while true; do
-    innernet $INNERNET_ARGS up --no-write-hosts "$INTERFACE"
+    if [[ $CLIENT_ARGS =~ --verbose ]]; then
+        innernet $INNERNET_ARGS up --no-write-hosts "$INTERFACE"
+    else
+        innernet $INNERNET_ARGS up --no-write-hosts "$INTERFACE" > /dev/null
+    fi
     sleep 1
 done
