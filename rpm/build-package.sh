f8f1f54b734d30d25eab21819abaf193d8d76470
Emmanuel Ortiz <eos175@gmail.com>
2024-12-15 21:25:39 -0600

    Fix Dockerfile for arm64 compatibility

--- rpm/build-package.sh
+++ rpm/build-package.sh
@@ -11,6 +11,7 @@ if [ $# -lt 1 ]; then
   echo "Usage: $0 distro version (from docker hub), e.g:"
   echo
   echo "$0 fedora:33"
+  echo "$0 almalinux:9 aarch64"
   exit 1
 else
   SPLIT_ARG=(${1//:/ })
@@ -21,16 +22,28 @@ else
     echo "bad arg"
     exit 1
   fi
+
+  # Detect architecture, default to x86_64
+  ARCH=${2:-x86_64}
+  if [[ "$ARCH" != "x86_64" && "$ARCH" != "aarch64" ]]; then
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+  fi
 fi
 
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $SCRIPT_DIR/..
 
-cmd docker build --tag "innernet-rpm-$DISTRO$VER" --file rpm/Dockerfile --build-arg DISTRO=$DISTRO --build-arg VER=$VER .
+cmd docker build \
+  --tag "innernet-rpm-$DISTRO$VER-$ARCH" \
+  --file rpm/Dockerfile \
+  --build-arg DISTRO=$DISTRO \
+  --build-arg VER=$VER \
+  --build-arg ARCH=$ARCH .
 
 echo "exporting built rpm's from docker image"
-cmd docker run --rm innernet-rpm-$DISTRO$VER sh -c "tar cf - target/rpm/*" | tar xv
+cmd docker run --rm "innernet-rpm-$DISTRO$VER-$ARCH" sh -c "tar cf - target/rpm/*" | tar xv
 
 echo "cleaning up"
 cmd docker image prune --force --filter label=stage=innernet-rpm
