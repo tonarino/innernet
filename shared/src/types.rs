ce247a1ec96bf38cd39c278d6bacb8a71de8f281
Marek Bernat <mbernat@users.noreply.github.com>
2025-08-05 13:28:56 +0200

    Explain what an unspecified IP address is

--- shared/src/types.rs
+++ shared/src/types.rs
@@ -450,11 +450,10 @@ pub struct ListenPortOpts {
 
 #[derive(Debug, Clone, PartialEq, Eq, Args)]
 pub struct OverrideEndpointOpts {
-    /// The external endpoint that you'd like the innernet server to broadcast
-    /// to other peers. The IP address may be unspecified, in which case the
-    /// server will try to resolve it based on its most recent connection.
-    /// The port will still be used even if you decide to use the unspecified
-    /// IP address.
+    /// The external endpoint that you'd like the innernet server to broadcast to other peers. The
+    /// IP address may be unspecified (all zeros), in which case the server will try to resolve it
+    /// based on its most recent connection. The port will still be used, even if you decide to use
+    /// an unspecified IP address.
     #[clap(short, long)]
     pub endpoint: Option<Endpoint>,
 
