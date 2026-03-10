28f61c91c10bd8eec3ca0f3dadb005d6e0b5f083
Marek Bernat <mbernat@users.noreply.github.com>
2026-03-10 16:16:20 +0100

    Add a custom error type

--- client-core/src/peer.rs
+++ client-core/src/peer.rs
@@ -1,25 +1,39 @@
 pub use innernet_shared::peer::NewPeerInfo;
 
-use crate::{rest_client::RestClient, Cidr, Peer, PeerInvitation};
+use crate::{
+    rest_client::{RestClient, RestError},
+    Cidr, Peer, PeerInvitation,
+};
 use anyhow::Result;
 use innernet_shared::{
     interface_config::{InterfaceConfig, InterfaceInfo, ServerInfo},
     CidrTree,
 };
 use std::path::Path;
+use thiserror::Error;
 use wireguard_control::{InterfaceName, KeyPair};
 
+#[derive(Debug, Error)]
+pub enum CreatePeerError {
+    // TODO(mbernat): Use a custom error type in the InterfaceConfig methods.
+    #[error("Error accessing innernet interface config file: {0}")]
+    InterfaceConfigAccess(anyhow::Error),
+    #[error("Error making a REST request: {0}")]
+    RestRequest(#[from] RestError),
+    #[error("Root CIDR prefix is longer than the new peer IP. Trying to use IPv4 address on an IPv6 CIDR?")]
+    PeerIpPrefixMismatch,
+}
+
 /// Create a new innernet [`Peer`] and a [`PeerInvitation`] they can use to join the network.
-//
-//  TODO: custom error type
 pub fn create_peer(
     config_dir: &Path,
     interface: &InterfaceName,
     cidrs: &[Cidr],
     peers: &[Peer],
     new_peer_info: NewPeerInfo,
-) -> Result<(Peer, PeerInvitation)> {
-    let interface_config = InterfaceConfig::from_interface(config_dir, interface)?;
+) -> Result<(Peer, PeerInvitation), CreatePeerError> {
+    let interface_config = InterfaceConfig::from_interface(config_dir, interface)
+        .map_err(CreatePeerError::InterfaceConfigAccess)?;
     let rest_client = RestClient::new(&interface_config.server);
 
     let keypair = KeyPair::generate();
@@ -27,7 +41,9 @@ pub fn create_peer(
     let peer = rest_client.create_peer(&peer_contents)?;
 
     let cidr_tree = CidrTree::new(cidrs);
-    let address = &cidr_tree.ip_net_for(peer.ip)?;
+    let address = &cidr_tree
+        .ip_net_for(peer.ip)
+        .map_err(|_| CreatePeerError::PeerIpPrefixMismatch)?;
     let interface_info = InterfaceInfo::new(interface, &keypair, address);
 
     let server_peer = peers.iter().find(|p| p.id == 1).unwrap();
