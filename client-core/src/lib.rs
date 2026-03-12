5874794bed1a35f5f14924eb9df6e7e6200b4167
Marek Bernat <mbernat@users.noreply.github.com>
2026-03-12 16:33:28 +0100

    Introduce a core Context type

--- client-core/src/lib.rs
+++ client-core/src/lib.rs
@@ -8,9 +8,15 @@ pub use innernet_shared::{
 };
 pub use wireguard_control::Backend;
 
+use crate::rest_client::RestClient;
 use anyhow::Error;
-use innernet_shared::wg;
-use std::path::Path;
+use innernet_shared::{
+    interface_config::{InterfaceConfig, InterfaceInfo, ServerInfo},
+    wg,
+};
+use std::path::PathBuf;
+use ureq::Agent;
+use wireguard_control::InterfaceName;
 
 pub mod data_store;
 pub mod interface;
@@ -25,19 +31,56 @@ pub const DEFAULT_DATA_DIR: &str = "/var/lib/innernet";
 #[cfg(target_os = "openbsd")]
 pub const DEFAULT_DATA_DIR: &str = "/var/db/innernet";
 
+/// [`Context`] contains data required by most public API functions.
+/// - [`Self::interface_info()`] produces an [`InterfaceInfo`] about an innernet interface.
+/// - [`Self::server_info()`] produces a [`ServerInfo`] about an innernet server.
+/// - [`Self::rest_client()`] produces a [`RestClient`] that can talk to an innernet server.
+pub struct Context {
+    config_dir: PathBuf,
+    interface: InterfaceName,
+    interface_config: InterfaceConfig,
+    agent: Agent,
+}
+
+impl Context {
+    // TODO(mbernat): Use a custom error type in the InterfaceConfig methods.
+    pub fn new(config_dir: PathBuf, interface: interface::InterfaceName) -> Result<Self, Error> {
+        let interface_config = InterfaceConfig::from_interface(&config_dir, &interface)?;
+        let agent = RestClient::create_agent();
+
+        Ok(Self {
+            config_dir,
+            interface,
+            interface_config,
+            agent,
+        })
+    }
+
+    pub fn interface_info(&self) -> &InterfaceInfo {
+        &self.interface_config.interface
+    }
+
+    pub fn server_info(&self) -> &ServerInfo {
+        &self.interface_config.server
+    }
+
+    pub fn rest_client(&self) -> RestClient<'_> {
+        RestClient::new(&self.agent, &self.interface_config.server)
+    }
+}
+
 /// Set the `listen_port`. `None` value unsets it.
 pub fn set_listen_port(
+    context: &mut Context,
     network_backend: Backend,
-    config_dir: &Path,
-    interface: &interface::InterfaceName,
-    config: &mut interface::InterfaceConfig,
     listen_port: Option<u16>,
 ) -> Result<(), Error> {
-    wg::set_listen_port(interface, listen_port, network_backend)?;
+    wg::set_listen_port(&context.interface, listen_port, network_backend)?;
     log::info!("the wireguard interface is updated");
 
+    let config = &mut context.interface_config;
     config.interface.listen_port = listen_port;
-    config.save(config_dir, interface)?;
+    config.save(&context.config_dir, &context.interface)?;
     log::info!("the config file is updated");
 
     Ok(())
--- client-core/src/peer.rs
+++ client-core/src/peer.rs
@@ -1,23 +1,13 @@
 pub use innernet_shared::peer::NewPeerInfo;
 
-use crate::{
-    rest_client::{RestClient, RestError},
-    Cidr, Peer, PeerInvitation,
-};
+use crate::{rest_client::RestError, Cidr, Context, Peer, PeerInvitation};
 use anyhow::Result;
-use innernet_shared::{
-    interface_config::{InterfaceConfig, InterfaceInfo, ServerInfo},
-    CidrTree,
-};
-use std::path::Path;
+use innernet_shared::{interface_config::InterfaceInfo, CidrTree};
 use thiserror::Error;
-use wireguard_control::{InterfaceName, KeyPair};
+use wireguard_control::KeyPair;
 
 #[derive(Debug, Error)]
 pub enum CreatePeerError {
-    // TODO(mbernat): Use a custom error type in the InterfaceConfig methods.
-    #[error("Error accessing innernet interface config file: {0}")]
-    InterfaceConfigAccess(anyhow::Error),
     #[error("Error making a REST request: {0}")]
     RestRequest(#[from] RestError),
     #[error("Root CIDR prefix is longer than the new peer IP. Trying to use IPv4 address on an IPv6 CIDR?")]
@@ -26,28 +16,22 @@ pub enum CreatePeerError {
 
 /// Create a new innernet [`Peer`] and a [`PeerInvitation`] they can use to join the network.
 pub fn create_peer(
-    config_dir: &Path,
-    interface: &InterfaceName,
+    context: &Context,
     cidrs: &[Cidr],
-    peers: &[Peer],
     new_peer_info: NewPeerInfo,
 ) -> Result<(Peer, PeerInvitation), CreatePeerError> {
-    let interface_config = InterfaceConfig::from_interface(config_dir, interface)
-        .map_err(CreatePeerError::InterfaceConfigAccess)?;
-    let rest_client = RestClient::new(&interface_config.server);
-
     let keypair = KeyPair::generate();
     let peer_contents = new_peer_info.into_peer_contents(&keypair);
-    let peer = rest_client.create_peer(&peer_contents)?;
+    let peer = context.rest_client().create_peer(&peer_contents)?;
 
     let cidr_tree = CidrTree::new(cidrs);
     let address = &cidr_tree
         .ip_net_for(peer.ip)
         .map_err(|_| CreatePeerError::PeerIpPrefixMismatch)?;
-    let interface_info = InterfaceInfo::new(interface, &keypair, address);
+    let interface_info = InterfaceInfo::new(&context.interface, &keypair, address);
 
-    let server_peer = peers.iter().find(|p| p.id == 1).unwrap();
-    let server_info = ServerInfo::new(server_peer, &interface_config.server.internal_endpoint);
-
-    Ok((peer, PeerInvitation::new(interface_info, server_info)))
+    Ok((
+        peer,
+        PeerInvitation::new(interface_info, context.server_info().clone()),
+    ))
 }
